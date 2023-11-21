c79823ae01c930a832524b19ed4d71fbffbb1403
Amireon <534811912@qq.com>
2023-11-21 21:44:52 +0800

    modify config

--- site.config.ts
+++ site.config.ts
@@ -1,92 +1,94 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://valaxy.site/',
   lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
+  title: '',
+  url: 'https://github.com/Amireon',
   author: {
-    name: '云游君',
+    name: 'Amireon',
+    avatar: 'https://avatars.githubusercontent.com/u/80996151?v=4',
   },
-  description: 'Valaxy Theme Yun Preview.',
+  subtitle: '',
+  description: '总归是在往前',
   social: [
-    {
-      name: 'RSS',
-      link: '/atom.xml',
-      icon: 'i-ri-rss-line',
-      color: 'orange',
-    },
-    {
-      name: 'QQ 群 1050458482',
-      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
-      icon: 'i-ri-qq-line',
-      color: '#12B7F5',
-    },
+    // {
+    //   name: 'RSS',
+    //   link: '/atom.xml',
+    //   icon: 'i-ri-rss-line',
+    //   color: 'orange',
+    // },
     {
       name: 'GitHub',
-      link: 'https://github.com/YunYouJun',
+      link: 'https://github.com/Amireon',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
-    {
-      name: '微博',
-      link: 'https://weibo.com/jizhideyunyoujun',
-      icon: 'i-ri-weibo-line',
-      color: '#E6162D',
-    },
-    {
-      name: '豆瓣',
-      link: 'https://www.douban.com/people/yunyoujun/',
-      icon: 'i-ri-douban-line',
-      color: '#007722',
-    },
-    {
-      name: '网易云音乐',
-      link: 'https://music.163.com/#/user/home?id=247102977',
-      icon: 'i-ri-netease-cloud-music-line',
-      color: '#C20C0C',
-    },
-    {
-      name: '知乎',
-      link: 'https://www.zhihu.com/people/yunyoujun/',
-      icon: 'i-ri-zhihu-line',
-      color: '#0084FF',
-    },
-    {
-      name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/1579790',
-      icon: 'i-ri-bilibili-line',
-      color: '#FF8EB3',
-    },
-    {
-      name: '微信公众号',
-      link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-      icon: 'i-ri-wechat-2-line',
-      color: '#1AAD19',
-    },
+    // {
+    //   name: 'QQ 群 1050458482',
+    //   link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
+    //   icon: 'i-ri-qq-line',
+    //   color: '#12B7F5',
+    // },
+    // {
+    //   name: '微博',
+    //   link: 'https://weibo.com/jizhideyunyoujun',
+    //   icon: 'i-ri-weibo-line',
+    //   color: '#E6162D',
+    // },
+    // {
+    //   name: '豆瓣',
+    //   link: 'https://www.douban.com/people/yunyoujun/',
+    //   icon: 'i-ri-douban-line',
+    //   color: '#007722',
+    // },
+    // {
+    //   name: '网易云音乐',
+    //   link: 'https://music.163.com/#/user/home?id=247102977',
+    //   icon: 'i-ri-netease-cloud-music-line',
+    //   color: '#C20C0C',
+    // },
+    // {
+    //   name: '知乎',
+    //   link: 'https://www.zhihu.com/people/yunyoujun/',
+    //   icon: 'i-ri-zhihu-line',
+    //   color: '#0084FF',
+    // },
+    // {
+    //   name: '哔哩哔哩',
+    //   link: 'https://space.bilibili.com/1579790',
+    //   icon: 'i-ri-bilibili-line',
+    //   color: '#FF8EB3',
+    // },
+    // {
+    //   name: '微信公众号',
+    //   link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
+    //   icon: 'i-ri-wechat-2-line',
+    //   color: '#1AAD19',
+    // },
     {
       name: 'Twitter',
-      link: 'https://twitter.com/YunYouJun',
+      link: 'https://twitter.com/Amireon',
       icon: 'i-ri-twitter-line',
       color: '#1da1f2',
     },
-    {
-      name: 'Telegram Channel',
-      link: 'https://t.me/elpsycn',
-      icon: 'i-ri-telegram-line',
-      color: '#0088CC',
-    },
+    // {
+    //   name: 'Telegram Channel',
+    //   link: 'https://t.me/elpsycn',
+    //   icon: 'i-ri-telegram-line',
+    //   color: '#0088CC',
+    // },
     {
       name: 'E-Mail',
-      link: 'mailto:me@yunyoujun.cn',
+      link: 'yyycwe@qq.com',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
     },
-    {
-      name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
-      icon: 'i-ri-train-line',
-      color: 'var(--va-c-text)',
-    },
+    // {
+    //   name: 'Travelling',
+    //   link: 'https://www.travellings.cn/go.html',
+    //   icon: 'i-ri-train-line',
+    //   color: 'var(--va-c-text)',
+    // },
   ],
 
   search: {
@@ -94,7 +96,7 @@ export default defineSiteConfig({
   },
 
   sponsor: {
-    enable: true,
+    enable: false,
     title: '我很可爱，请给我钱！',
     methods: [
       {
--- valaxy.config.ts
+++ valaxy.config.ts
@@ -17,31 +17,31 @@ export default defineValaxyConfig<UserThemeConfig>({
   themeConfig: {
     banner: {
       enable: true,
-      title: '云游君的小站',
+      title: '因你而在的故事',
       cloud: {
         enable: true,
       },
     },
 
     pages: [
-      {
-        name: '我的小伙伴们',
-        url: '/links/',
-        icon: 'i-ri-genderless-line',
-        color: 'dodgerblue',
-      },
-      {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
-      },
+      // {
+      //   name: '我的小伙伴们',
+      //   url: '/links/',
+      //   icon: 'i-ri-genderless-line',
+      //   color: 'dodgerblue',
+      // },
+      // {
+      //   name: '喜欢的女孩子',
+      //   url: '/girls/',
+      //   icon: 'i-ri-women-line',
+      //   color: 'hotpink',
+      // },
     ],
 
     footer: {
       since: 2016,
       beian: {
-        enable: true,
+        enable: false,
         icp: '苏ICP备17038157号',
       },
     },
